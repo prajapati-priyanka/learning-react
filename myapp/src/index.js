41bfa68a3e3b7b9c3cb1b412d731aa77cb598a72
Priyanka-Prajapati <contact.priyankaprajapati@gmail.com>
2021-06-03 00:04:27 +0530

    initial commit

---
+++ myapp/src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+ReactDOM.render(
+
+  <>
+  <div className= "App">
+    <div className= "Navbar">
+     <h1>The Priyanka's Blog</h1>
+     <div className="links">
+       <a href="/">Home</a>
+       <a href="/">Blog</a>
+     </div>
+
+    </div>
+    <div className= "content">
+      <h2>Main/Home Page</h2>
+    </div>
+    <div>
+
+    </div>
+  </div>
+  </>
+  , document.getElementById('root'));
