3fd2df86c58da5dce360e7858234a408b39d3402
Priyanka-Prajapati <contact.priyankaprajapati@gmail.com>
2021-06-06 00:37:49 +0530

    refractor: changed blog to new blog

--- myapp/src/Navbar.js
+++ myapp/src/Navbar.js
@@ -7,7 +7,7 @@ function Navbar(){
         <h1>My Blog</h1>
         <div className="links">
           <Link to="/">Home</Link>
-          <Link to="/create">Blog</Link>
+          <Link to="/create">New Blog</Link>
         </div>
         
         </div>
