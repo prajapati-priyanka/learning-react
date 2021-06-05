ab608272af7ca621bb94147f3d11ee28ec300a83
Priyanka-Prajapati <contact.priyankaprajapati@gmail.com>
2021-06-05 13:28:28 +0530

    feat: added error handler

--- myapp/src/Home.js
+++ myapp/src/Home.js
@@ -4,22 +4,39 @@ import Bloglist from "./Bloglist";
 function Home() {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
-  useEffect(()=>{
-   //   This Is just to show the loading until the fetch call happens. It is not reccomended.
-     setTimeout(()=>{
-      fetch(' http://localhost:8000/blogs')
-      .then(response => response.json())
-      .then(data => {
-         setBlogs(data);
-         setIsPending(false);
-      },2000)
-     })
-  },[])
+  const [error, setError] = useState(null);
+  
+  useEffect(() => {
+    //   This Is just to show the loading until the fetch call happens. It is not reccomended.
+    setTimeout(() => {
+      fetch(" http://localhost:8000/blogs")
+        .then((response) =>{
+          // This error is to handle the wrong endpoints
+           if(!response.ok){
+             throw Error("Could not fetch the data from that source");
+           }
+          return response.json();
+      })
+
+        .then((data) => {
+          setBlogs(data);
+          setIsPending(false);
+          setError(null);
+       
+          })
+          // This is for network error or server is off
+          .catch(err => {
+            setIsPending(false)
+            setError(err.message);
+        }, 2000);
+    });
+  }, []);
 
   return (
     <div className="home">
-    {isPending && <div>Loading.....</div>}
-    { blogs && <Bloglist blogs={blogs} /> }  
+     {error && <div>{error}</div>}
+      {isPending && <div>Loading.....</div>}
+      {blogs && <Bloglist blogs={blogs} />}
     </div>
   );
 }
